9cb0b48ad35632a41a0bbc1fa16543e17e72292c
Delyan Raychev <az@delqn.com>
2013-08-09 03:56:20 -0700

    Game is in working condition

--- gen/com/bignerdranch/android/geoquiz/R.java
+++ gen/com/bignerdranch/android/geoquiz/R.java
@@ -24,9 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int false_button=0x7f080001;
-        public static final int true_button=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int button0=0x7f08000b;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f080006;
+        public static final int button6=0x7f080007;
+        public static final int button7=0x7f080008;
+        public static final int button8=0x7f080009;
+        public static final int button9=0x7f08000a;
+        public static final int numberEnterred=0x7f080001;
+        public static final int textHistory=0x7f080000;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
@@ -37,12 +47,18 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int correct_toast_string=0x7f050006;
-        public static final int false_button=0x7f050005;
+        public static final int button0=0x7f050004;
+        public static final int button1=0x7f050005;
+        public static final int button2=0x7f050006;
+        public static final int button3=0x7f050007;
+        public static final int button4=0x7f050008;
+        public static final int button5=0x7f050009;
+        public static final int button6=0x7f05000a;
+        public static final int button7=0x7f05000b;
+        public static final int button8=0x7f05000c;
+        public static final int button9=0x7f05000d;
         public static final int hello_world=0x7f050002;
         public static final int question_text=0x7f050003;
-        public static final int true_button=0x7f050004;
-        public static final int wrong_toast_string=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,44 +1,136 @@
 package com.bignerdranch.android.geoquiz;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
-	private Button mTrueButton;
-	private Button mFalseButton;
+	private String randomNumber = "";
+	private Integer tries = 0;
 	
+	private void makeAllButtonsVisible() {
+		findViewById(R.id.button0).setVisibility(View.VISIBLE);
+		findViewById(R.id.button1).setVisibility(View.VISIBLE);
+		findViewById(R.id.button2).setVisibility(View.VISIBLE);
+		findViewById(R.id.button3).setVisibility(View.VISIBLE);
+		findViewById(R.id.button4).setVisibility(View.VISIBLE);
+		findViewById(R.id.button5).setVisibility(View.VISIBLE);
+		findViewById(R.id.button6).setVisibility(View.VISIBLE);
+		findViewById(R.id.button7).setVisibility(View.VISIBLE);
+		findViewById(R.id.button8).setVisibility(View.VISIBLE);
+		findViewById(R.id.button9).setVisibility(View.VISIBLE);
+	}
+
+	private String thinkOfANumber() {
+		Random r = new Random();
+		String randomNumber = "";
+		while(randomNumber.length() < 4) {
+			Integer i = r.nextInt(9);
+			Log.d("random digit -->", i.toString());
+			String x = i.toString();
+			if(!randomNumber.contains(x)) {
+				randomNumber = TextUtils.concat(randomNumber, x).toString();
+			}
+		}
+		return randomNumber;
+	}
+
+	private int[] getCowsBulls(String guess) {
+		int[] cowsBulls = {0,0};
+		Log.d("random number = ", randomNumber.toString());
+		
+		char[] rn = randomNumber.toCharArray();
+		char[] gn = guess.toCharArray();
+		
+		for(int i=0; i<=3; ++i) {
+			int guessedDigit = gn[i];
+			if(guessedDigit == rn[i]) {
+				cowsBulls[1]++;
+			} else {
+				for(int j=0; j<=3; ++j) {
+					if(guessedDigit == rn[j]) cowsBulls[0]++;
+				}
+			}
+		}
+		return cowsBulls;
+	}
+	
+	private void wireButton(Button aButton) {
+		aButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Button theButton = (Button)v;
+				// TODO Auto-generated method stub
+				
+				theButton.setVisibility(View.INVISIBLE);
+				
+				//Toast.makeText(QuizActivity.this, theButton.getText().toString(), Toast.LENGTH_SHORT).show();
+				
+				final TextView mNumberEnterred = (TextView)findViewById(R.id.numberEnterred);
+				mNumberEnterred.setText(TextUtils.concat(mNumberEnterred.getText(), theButton.getText()));
+				
+				if(mNumberEnterred.getText().length()==4) {
+					tries++;
+					int[] cowsBulls = getCowsBulls(mNumberEnterred.getText().toString());
+					final TextView mTextHistory = (TextView)findViewById(R.id.textHistory);
+					Integer cows = cowsBulls[0];
+					Integer bulls = cowsBulls[1];
+					mTextHistory.setText(TextUtils.concat(mTextHistory.getText(),
+							tries.toString(), ": ", 
+							mNumberEnterred.getText(),
+							"  cows:", cows.toString(),
+							"  bulls:", bulls.toString(), "\n"));
+					mNumberEnterred.setText("");					
+					makeAllButtonsVisible();
+					if(bulls==4) {
+						Toast.makeText(QuizActivity.this,
+								TextUtils.concat("Congrats!  You won after ", tries.toString(), " tries"),
+								Toast.LENGTH_SHORT).show();
+						initGame();
+					}
+				}
+			}
+		});
+	}
+
+	
+	private void initGame() {
+		((TextView)findViewById(R.id.numberEnterred)).setText("");
+		((TextView)findViewById(R.id.textHistory)).setText("");
+		randomNumber = thinkOfANumber();
+        ((TextView)findViewById(R.id.textHistory)).setText(TextUtils.concat("random number: ", randomNumber, "\n"));
+		makeAllButtonsVisible(); // just in case, lol
+		
+        this.wireButton((Button)findViewById(R.id.button0));
+        this.wireButton((Button)findViewById(R.id.button1));
+        this.wireButton((Button)findViewById(R.id.button2));
+        this.wireButton((Button)findViewById(R.id.button3));
+        this.wireButton((Button)findViewById(R.id.button4));
+        this.wireButton((Button)findViewById(R.id.button5));
+        this.wireButton((Button)findViewById(R.id.button6));
+        this.wireButton((Button)findViewById(R.id.button7));
+        this.wireButton((Button)findViewById(R.id.button8));
+        this.wireButton((Button)findViewById(R.id.button9));
+	}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        
-        mTrueButton = (Button)findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.correct_toast_string, Toast.LENGTH_SHORT).show();
-			}
-		});
-        
-        mFalseButton = (Button)findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.wrong_toast_string, Toast.LENGTH_SHORT).show();
-			}
-		});
+
+        initGame();
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
